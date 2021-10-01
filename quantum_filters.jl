45ff50848e44044dd521494dcfd34208c332ebac
Lteins <sihan@datachat.ai>
2021-09-30 23:45:33 -0700

    first

---
+++ quantum_filters.jl
@@ -0,0 +1,50 @@
+include("dkk17.jl")
+
+function rcov_quantum_filter(reps, eps, k, α=4, τ=0.1; limit1=2, limit2=1.5)
+    d, n = size(reps)
+    reps_pca, U = pca(reps, k)
+    if k == 1
+        reps_estimated_white = reps_pca
+        Σ′ = ones(1, 1)
+    else
+        selected = cov_estimation_iterate(reps_pca, eps/n, τ, nothing, limit=round(Int, limit1*eps))
+        reps_pca_selected = reps_pca[:, selected]
+        Σ = cov(reps_pca_selected', corrected=false)
+        reps_estimated_white = Σ^(-1/2)*reps_pca
+        Σ′ = cov(reps_estimated_white')
+    end
+    M = k > 1 ? exp(α*(Σ′- I)/(opnorm(Σ′) - 1)) : ones(1, 1)
+    M /= tr(M)
+    estimated_poison_ind = k_lowest_ind(
+        -[x'M*x for x in eachcol(reps_estimated_white)],
+        round(Int, limit2*eps)
+    )
+    return .! estimated_poison_ind
+end
+
+function rcov_auto_quantum_filter(polluted_mark, reps, eps, α=0, τ=0.1; limit1=2, limit2=1.5)
+    n = size(reps)[2]
+    reps_pca, U = pca(reps, 5)
+    best_opnorm, best_selected, best_k = -Inf, nothing, nothing
+    pr = 0
+    for k in round.(Int, range(1, sqrt(5), length=10) .^ 2)
+        selected = rcov_quantum_filter(reps, eps, k, α, τ; limit1=limit1, limit2=limit2)
+        Σ = cov(reps_pca[:, selected]')
+        Σ′ = cov((Σ^(-1/2)*reps_pca)')
+        M = exp(α*(Σ′- I)/(opnorm(Σ′) - 1))
+        M /= tr(M)
+        op = tr(Σ′ * M) / tr(M)
+        pr = 0
+        for i in 1:n
+            if polluted_mark[i] && !selected[i]
+                pr += 1
+            end
+        end
+        @show k, op, pr
+        if op > best_opnorm
+            best_opnorm, best_selected, best_k = op, selected, k
+        end
+    end
+    @show best_k, best_opnorm
+    return best_selected
+end
---
+++ run_filters.jl
@@ -0,0 +1,47 @@
+using Printf
+using NPZ
+include("util.jl")
+include("kmeans_filters.jl")
+include("quantum_filters.jl")
+
+log_file = open("run_filters.log", "a")
+
+# for name in ARGS
+    # target_label = parse(Int, split(name, "-")[3][end:end])
+name = "test"
+reps = npzread("ones")'
+n = size(reps)[2]
+polluted_mark = npzread("mark")
+eps = sum(polluted_mark)
+removed = round(Int, 1.5*eps)
+
+    # @printf("%s: Running PCA filter\n", name)
+    # reps_pca, U = pca(reps, 1)
+    # pca_poison_ind = k_lowest_ind(-abs.(mean(reps_pca[1, :]) .- reps_pca[1, :]), round(Int, 1.5*eps))
+    # poison_removed = sum(pca_poison_ind[end-eps+1:end])
+    # clean_removed = removed - poison_removed
+    # @show poison_removed, clean_removed
+    # @printf(log_file, "%s-pca: %d, %d\n", name, poison_removed, clean_removed)
+    # npzwrite("output/$(name)/mask-pca-target.npy", pca_poison_ind)
+
+
+    # @printf("%s: Running kmeans filter\n", name)
+    # kmeans_poison_ind = .! kmeans_filter2(reps, eps)
+    # poison_removed = sum(kmeans_poison_ind[end-eps+1:end])
+    # clean_removed = removed - poison_removed
+    # @show poison_removed, clean_removed
+    # @printf(log_file, "%s-kmeans: %d, %d\n", name, poison_removed, clean_removed)
+    # npzwrite("output/$(name)/mask-kmeans-target.npy", kmeans_poison_ind)
+
+@printf("Running quantum filter\n")
+quantum_poison_ind = .! rcov_auto_quantum_filter(polluted_mark, reps, eps)
+# quantum_poison_ind = .! rcov_quantum_filter(reps, eps, 3)
+pr = 0
+for i in 1:n
+    if polluted_mark[i] && quantum_poison_ind[i]
+        global pr += 1
+    end
+end    
+clean_removed = removed - pr
+@show pr, clean_removed, eps
+# end
