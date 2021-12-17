cd2b9626e4387d7d05bd36606cb4ce7ebc558060
Lteins <leo990629@gmail.com>
2021-12-17 12:50:29 -0800

    Update run_filters.jl

--- run_filters.jl
+++ run_filters.jl
@@ -9,9 +9,9 @@ log_file = open("run_filters.log", "a")
 # for name in ARGS
     # target_label = parse(Int, split(name, "-")[3][end:end])
 name = "test"
-reps = npzread("/content/gdrive/My Drive/Resnet/Cheat/ones(v3).np")'
+reps = npzread("/content/gdrive/My Drive/Resnet/Cheat/ones(v2).np")'
 n = size(reps)[2]
-polluted_mark = npzread("/content/gdrive/My Drive/Resnet/Cheat/mark(v3).np")
+polluted_mark = npzread("/content/gdrive/My Drive/Resnet/Cheat/mark(v2).np")
 eps = sum(polluted_mark)
 # removed = round(Int, 1.5*eps)
 removed = round(Int, 1.5*3424)
